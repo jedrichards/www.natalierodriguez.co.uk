84d41a350ddb51597e1b631a0ab27523f16cc9d3
Jed Richards <jedrichards@gmail.com>
2015-09-13 12:28:37 +0100

    Change pics

--- routes/sub-page.js
+++ routes/sub-page.js
@@ -2,6 +2,10 @@ module.exports = {
     method: 'GET',
     path: '/{page}',
     handler: function (request, reply) {
+        if (request.path === '/favicon.ico') {
+          reply(404);
+          return;
+        }
         reply.view('sub-pages/' + request.params.page, {
             subPage: true
         });
